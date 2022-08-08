e586171661cfc7e6ea6799d9ea5b04ce42a58e79
toilathor <lequangtho2000lqtho@gmail.com>
2022-08-08 15:55:35 +0700

    infinite list app bloc

--- bloc_example/lib/main.dart
+++ bloc_example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bloc/bloc.dart';
-import 'package:bloc_example/app/counter_app.dart';
+import 'package:bloc_example/app/infinite_list_app.dart';
 import 'package:bloc_example/counter_observer.dart';
-import 'package:bloc_example/app/timer_app.dart';
 import 'package:flutter/widgets.dart';
 
 // void main() {
@@ -11,9 +10,16 @@ import 'package:flutter/widgets.dart';
 //   );
 // }
 
+// void main() {
+//   BlocOverrides.runZoned(
+//     () => runApp(const TimerApp()),
+//     blocObserver: CounterObserver(),
+//   );
+// }
+
 void main() {
   BlocOverrides.runZoned(
-    () => runApp(const TimerApp()),
+    () => runApp(const InfiniteListApp()),
     blocObserver: CounterObserver(),
   );
 }
