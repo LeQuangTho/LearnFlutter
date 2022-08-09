0d81fd6e52d45d2d3b71ee2d3547b3f3ddc26b07
toilathor <lequangtho2000lqtho@gmail.com>
2022-08-09 11:04:05 +0700

    login app bloc

---
+++ bloc_example/lib/bloc/login/login_bloc.dart
@@ -0,0 +1,56 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:formz/formz.dart';
+
+import '../../models/models.dart';
+
+part 'login_event.dart';
+
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  LoginBloc(this._authenticationRepository) : super(const LoginState()) {
+    on<LoginUsernameChanged>(_onUsernameChange);
+    on<LoginPasswordChanged>(_onPasswordChange);
+    on<LoginSubmitted>(_onSubmitted);
+  }
+
+  final AuthenticationRepository _authenticationRepository;
+
+  void _onUsernameChange(LoginUsernameChanged event, Emitter<LoginState> emit) {
+    final username = Username.dirty(event.username);
+    emit(
+      state.copyWith(
+        username: username,
+        status: Formz.validate([state.password, username]),
+      ),
+    );
+  }
+
+  void _onPasswordChange(LoginPasswordChanged event, Emitter<LoginState> emit) {
+    final password = Password.dirty(event.password);
+    emit(
+      state.copyWith(
+        password: password,
+        status: Formz.validate([state.password, state.username]),
+      ),
+    );
+  }
+
+  Future<void> _onSubmitted(
+      LoginSubmitted event, Emitter<LoginState> emit) async {
+    if (state.status.isValidated) {
+      emit(state.copyWith(status: FormzStatus.submissionInProgress));
+      try {
+        await _authenticationRepository.logIn(
+          username: state.username.value,
+          password: state.password.value,
+        );
+        emit(state.copyWith(status: FormzStatus.submissionSuccess));
+      } catch (_) {
+        emit(state.copyWith(status: FormzStatus.submissionFailure));
+      }
+    }
+  }
+}
--- bloc_example/lib/main.dart
+++ bloc_example/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:authentication_repository/authentication_repository.dart';
 import 'package:bloc/bloc.dart';
-import 'package:bloc_example/app/infinite_list_app.dart';
+import 'package:bloc_example/app/login_app.dart';
 import 'package:bloc_example/counter_observer.dart';
 import 'package:flutter/widgets.dart';
+import 'package:user_repository/user_repository.dart';
 
 // void main() {
 //   BlocOverrides.runZoned(
@@ -21,5 +23,11 @@ void main() {
   Bloc.observer = CounterObserver();
   // runApp(const CounterApp());
   // runApp(const TimerApp());
-  runApp(const InfiniteListApp());
+  // runApp(const InfiniteListApp());
+  runApp(
+    LoginApp(
+      authenticationRepository: AuthenticationRepository(),
+      userRepository: UserRepository(),
+    ),
+  );
 }
